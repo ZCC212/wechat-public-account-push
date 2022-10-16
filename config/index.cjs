1971dacefaf09714140bf47e18a30e7852e7c006
zcc <80560633+ZCC212@users.noreply.github.com>
2022-10-16 12:11:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,43 +8,43 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxb79e270e448063f0',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '5d28026d8f9c71bff16bfc18c09be6f4',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖南',
+  CITY: '长沙',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝宝咪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oQ4Gp5vvdPpG1ZJR1VySPGBkMkSw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'YHgfV3G54YJLBARoLLrf9dbPgp9z5P9UeBp9bwSejBU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+//         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝宝咪', year: '2000', date: '02-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2020', date: '11-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-03' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'YHgfV3G54YJLBARoLLrf9dbPgp9z5P9UeBp9bwSejBU',
     }
   ],
 
