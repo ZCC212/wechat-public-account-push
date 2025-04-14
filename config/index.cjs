223eb36794a462c0101fe4dbeae11f16b77575b9
zcc <80560633+ZCC212@users.noreply.github.com>
2025-04-14 18:13:15 +0800

    修改为自己

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ojvM-6pxQwkWc3LYy9W6aBpTLVgg',
+      // id: 'ojvM-6pxQwkWc3LYy9W6aBpTLVgg',
+      id: 'ojvM-6lHk9GCumYK5cDk_2NVVTVg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1EoEL9L8g9wwHLBNKBhg93Ybhe4_UtIQ5m7raa1QNCs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
