f04e139008c8ed0d6985b675b9f89ab3f5ac4a24
zcc <80560633+ZCC212@users.noreply.github.com>
2025-04-14 17:41:50 +0800

    修改模版id

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ojvM-6pxQwkWc3LYy9W6aBpTLVgg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '62E8zyVY14qVcwXRP11BfMV1km8JccV7DbdgalcSYZc',
+      useTemplateId: 'bsVTKGzbXP7GfQSsM4pz_xEw9zsuUYwU8xZAEmaW5bY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-17',
       festivals: [
